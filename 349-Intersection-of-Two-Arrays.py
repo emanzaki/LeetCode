2619f46b75456d8fa66c5c8fc1425b71e60bc263
Eman Zaki <88564710+emanzaki@users.noreply.github.com>
2024-09-17 17:17:24 +0300

    [349] [Time Beats: 82.79%] [Memory Beats: 24.17%] - LeetPush

--- 349-Intersection-of-Two-Arrays.py
+++ 349-Intersection-of-Two-Arrays.py
@@ -1,7 +1,7 @@
 class Solution:
     def intersection(self, nums1: List[int], nums2: List[int]) -> List[int]:
-        num1 = set(nums1)
-        num2 = set(nums2)
+        nums1 = set(nums1)
+        nums2 = set(nums2)
         arr =[]
         for x in nums1:
             if x in nums2:
